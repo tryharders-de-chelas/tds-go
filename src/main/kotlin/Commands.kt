407a2fc442410ae53803728eb368afc689d03ffb
MarioRj16 <mariorijocarvalho@gmail.com>
2023-10-28 13:47:10 +0000

    commands added 0.1

--- src/main/kotlin/Commands.kt
+++ src/main/kotlin/Commands.kt
@@ -1,12 +1,14 @@
+import kotlin.system.exitProcess
+
 fun receive(input:String){
     var splittedInput=input.split(" ")
     when(splittedInput[0]){
         "new" -> create()
         "play" -> move(splittedInput[1])
         "pass" -> move(null)
-        "save" -> saveBoard()
-        "load" -> loadBoard()
-        "exit" -> exitProcess()
+        "save" -> saveBoard(splittedInput[1])
+        "load" -> loadBoard(splittedInput[1])
+        "exit" -> exitProcess(0)
         else -> println("Invalid command $input")
     }
 
