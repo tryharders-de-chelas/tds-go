0ad0a584d2c42095c9f8aaf4a2077b75ee08f9f2
Rxfa <rafa3rnrn@gmail.com>
2023-10-28 15:23:46 +0100

    wrote board.draw()

--- src/main/kotlin/model/Board.kt
+++ src/main/kotlin/model/Board.kt
@@ -1,7 +1,11 @@
 package model
 
 class Board {
-    val board = Array(BOARD_SIZE) { Array(BOARD_SIZE){ Cell() } }
+    private val board = Array(BOARD_SIZE) { Array(BOARD_SIZE){ Cell() } }
     val lines = board.size
+    fun draw(){
+        println(((0..<lines).map { 'A' + it }).joinToString( prefix = " ".repeat(2), separator = " "))
+        board.forEachIndexed { idx, line -> println(line.joinToString(prefix = "${lines - idx} ", separator = " ") { it.state.value }) }
+    }
 
 }
