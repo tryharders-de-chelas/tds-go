06f67f488856047e0dd259bf4225cdee0c6d0efc
MarioRj16 <mariorijocarvalho@gmail.com>
2023-10-28 15:12:40 +0000

    2 passes to end added

--- src/main/kotlin/Commands.kt
+++ src/main/kotlin/Commands.kt
@@ -1,20 +1,40 @@
+package model
 import kotlin.system.exitProcess
 import java.io.BufferedReader
 import java.io.FileReader
 import java.io.PrintWriter
+
+
 fun receive(input:String){
     var splittedInput=input.split(" ")
     when(splittedInput[0]){
         "new" -> create()
         "play" -> move(splittedInput[1])
-        "pass" -> move(null)
+        "pass" -> {
+            wantsToEnd++
+            if (wantsToEnd==2) endGame()
+            else move(null)
+        }
         "save" -> saveBoard(splittedInput[1])
         "load" -> loadBoard(splittedInput[1])
         "exit" -> exitProcess(0)
-        else -> println("Invalid command $input")
+        else -> {
+            println("Invalid command $input")
+            return
+        }
     }
+    if (splittedInput[0]!="pass") wantsToEnd=0
 
 }
+
+
+fun endGame(){
+    //show board
+    //GAME OVER and score
+
+}
+//pass 2 times draw
+//confirm with stack
 fun create(){
 
 }
