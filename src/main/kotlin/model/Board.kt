85dac68849d1ad1b6d98060918fc9612db154c2d
MarioRj16 <mariorijocarvalho@gmail.com>
2023-11-01 14:41:10 +0000

    2 passes to end added

--- src/main/kotlin/model/Board.kt
+++ src/main/kotlin/model/Board.kt
@@ -19,13 +19,47 @@ data class Board(
 
     private fun isLegal(position: Int) = isFree(position) && isInBounds(position)
 
-    private fun canCapture(): List<Cell>?{
-        //cantBeCaptured(position)
-        TODO()
+    private fun canCapture(position: Int): List<Cell>{
+        val cell=board[position]
+        val up = searchSurrond(cell.up())
+        val down =searchSurrond(cell.down())
+        val left = searchSurrond(cell.left())
+        val right =searchSurrond(cell.right())
+    return up+down+left+right
+    }
+
+    private fun searchSurrond(cell: Cell?):List<Cell>{
+        if (cell==null) return listOf()
+        return search(cell.id, listOf(board[cell.id]),player.other.state) ?: return listOf()
     }
     private fun isFree(position: Int) = board[position].state == State.FREE
 
-    private fun cantBeCaptured(position: Int){TODO()}
+
+    private fun cantBeCaptured(position: Int):Boolean{
+        return search(position,listOf(board[position]),player.state)==null
+    }
+
+    private fun search(position: Int,visited:List<Cell>,state:State):List<Cell>?{
+        val cell=board[position]
+        val up = confirmCell(cell.up(), visited,state) ?: return null
+        val down = confirmCell(cell.down(), visited,state) ?: return null
+        val left = confirmCell(cell.left(), visited,state) ?: return null
+        val right = confirmCell(cell.right(), visited,state) ?: return null
+        return up+down+left+right
+    }
+
+    private fun confirmCell(cell:Cell?,visited:List<Cell>,state:State):List<Cell>?{
+        if(cell !in visited){
+            if(cell?.state==State.FREE ){
+                return null
+            }else if(cell!=null && cell.state==state){
+                return search(cell.id,visited+cell,state)
+            }
+        }
+        return visited
+    }
+
+
     //TODO("Make it work for cases in which a group of more than 2 cells are surrounded")
 
     fun Cell.up() = if(board[id].row == 0) null else board[id - BOARD_SIZE]
@@ -40,11 +74,19 @@ data class Board(
     fun play(str: String): Board {
         val position = toPosition(str)
         require(isLegal(position))
-        val toRemove = canCapture()
-        return copy(
+        val toRemove = canCapture(position)
+        return if(toRemove.isNotEmpty()) copy(board=board.mapIndexed{idx,value->
+            when {
+                idx == position->Cell(position, player.state)
+                (Cell(idx,player.other.state) in toRemove) -> Cell(position,State.FREE)
+                else -> value
+            }
+        },turn=turn + 1
+        ) else if (cantBeCaptured(position)) copy(
             board=board.mapIndexed{idx, value -> if(idx == position) Cell(position, player.state) else value},
             turn = turn + 1
         )
+        else null
     }
 
     fun draw(){
